6cb9a18bf54ede977935c5d6e657e2d1af8f0bd3
Serkan Konakcı <Serkan.Konakci@tubitak.gov.tr>
2023-05-06 21:47:32 +0300

    build: implement home page design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Link {
+  path: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  links: Link[] = [
+    {
+      path: '/',
+      name: 'Home',
+    },
+    {
+      path: '/units',
+      name: 'Units',
+    },
+  ];
+}
--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
